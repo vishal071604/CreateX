0685016b76d919d4d6c7ee479a50ce0938c4c3ce
Vishal <vishalkalawad51@gmail.com>
2026-09-11 03:16:22 +0530

    Update frontend and backend

--- Backend/server.js
+++ Backend/server.js
@@ -16,7 +16,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://create-x-pearl.vercel.app/"],
     credentials: true,
   })
 );
